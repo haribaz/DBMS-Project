465077dab0621367a5b7cf2450b9f5635ab3b918
haribaz <rahuljaya007@gmail.com>
2021-11-15 00:00:16 +0530

    Real Complete Setup

--- index.js
+++ index.js
@@ -4,18 +4,23 @@ const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
 const methodOverride = require('method-override');
 const app = express();
+const path = require('path');
 
 console.log(process.env.MONGODB_URL);
 console.log(process.env.PORT);
 require('./src/database/setup.js');
 
 app.set('view engine', 'ejs');
-app.set('views', __dirname + '/views');
+app.set('views', path.join(__dirname + '/src/views'));
 app.set('layout', 'layouts/layout');
 app.use(expressLayouts);
 app.use(methodOverride('_method'));
 app.use(express.static('public'));
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ limit: '10mb', extended: false }));
+
+app.get('/', (req, res) => {
+	res.render('index');
+});
 
 app.listen(process.env.PORT || 3000);
